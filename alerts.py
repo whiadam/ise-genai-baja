312bb0ccbf6feaf86e6dc0fbd4e952988230126f
Danae Ludy <111144400+danaeludy@users.noreply.github.com>
2026-03-21 23:53:19 -0400

    Clean up alerts.py by removing unused code
    Removed commented-out code and example function for clarity.

--- alerts.py
+++ alerts.py
@@ -1,38 +1,10 @@
-#############################################################################
-# modules.py
-#
-# This file contains modules that may be used throughout the app.
-#
-# You will write these in Unit 2. Do not change the names or inputs of any
-# function other than the example.
-#############################################################################
+#################### Imports #####################################
 from __future__ import annotations  
 from datetime import datetime, date, time, timedelta  
 from typing import Any  
 
 import streamlit as st 
 from internals import create_component
-
-
-# This one has been written for you as an example. You may change it as wanted.
-def display_my_custom_component(value):
-    """Displays a 'my custom component' which showcases an example of how custom
-    components work.
-
-    value: the name you'd like to be called by within the app
-    """
-    # Define any templated data from your HTML file. The contents of
-    # 'value' will be inserted to the templated HTML file wherever '{{NAME}}'
-    # occurs. You can add as many variables as you want.
-    data = {
-        'NAME': value,
-    }
-    # Register and display the component by providing the data and name
-    # of the HTML file. HTML must be placed inside the "custom_components" folder.
-    html_file_name = "my_custom_component"
-    create_component(data, html_file_name)
-
-
 ##################### Danae's Section ############################
 def _ensure_alert_state() -> None:
     """
