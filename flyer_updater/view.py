280e4bd53ee90f313acd127a930e394e282f6708
jperparas <j.s.perparas@gmail.com>
2026-03-23 19:39:31 -0700

    fix import by setting relative path for landing_page

--- flyer_updater/view.py
+++ flyer_updater/view.py
@@ -1,6 +1,6 @@
 # use AI for templating
 import streamlit as st
-from landing_page import render_landing_page
+from .landing_page import render_landing_page
 tab1, tab2 = st.tabs(["Upload a Photo", "Flyer Log"])
 
 with tab1:
